60a60e2e448e0ba6aa911cdda3b2b472e7559bc9
mlandauer <devnull@localhost>
2007-11-23 16:02:13 +1100

    [svn] Why am I paying so much attention to something so small?

--- name.rb
+++ name.rb
@@ -2,14 +2,19 @@
 class Name
   attr_accessor :first, :last
   
-  def initialize(first, last)
-    @first = first.capitalize
-    @last = last.capitalize
+  def initialize(params)
+    @first = params[:first].capitalize if params[:first]
+    @last = params[:last].capitalize if params[:last]
+    throw "Invalid keys" unless (params.keys - [:first, :last]).empty?
   end
   
   def Name.last_title_first(text)
     names = text.delete(',').split(" ")
     throw "Unexpected number of names" if names.size != 2
-    Name.new(names[1], names[0])
+    Name.new(:last => names[0], :first => names[1])
+  end
+  
+  def ==(name)
+    @first == name.first && @last == name.last
   end
 end
--- name_test.rb
+++ name_test.rb
@@ -2,21 +2,34 @@ require 'test/unit'
 require 'name'
 
 class NameTest < Test::Unit::TestCase
-  def test_simple
-    name = Name.last_title_first("Gash Joanna")
-    assert_equal("Gash", name.last)
-    assert_equal("Joanna", name.first)
+  def setup
+    @matthew = Name.new(:first => "Matthew", :last => "Landauer")
+    @joanna_gash = Name.new(:first => "Joanna", :last => "Gash")
+  end
+  
+  def test_new
+    assert_equal("Matthew", @matthew.first)
+    assert_equal("Landauer", @matthew.last)
+  end
+  
+  def test_new_wrong_parameters
+    assert_raise(NameError){ Name.new(:first => "foo", :blah => "dibble") }
+  end
+  
+  def test_equals
+    assert_equal(@matthew, Name.new(:last => "Landauer", :first => "Matthew"))
+    assert_not_equal(@matthew, Name.new(:last => "Landauer"))
+  end
+  
+  def test_simple_parse
+    assert_equal(@joanna_gash, Name.last_title_first("Gash Joanna"))
   end
   
   def test_capitals
-    name = Name.last_title_first("GASH joanna")
-    assert_equal("Gash", name.last)
-    assert_equal("Joanna", name.first)
+    assert_equal(@joanna_gash, Name.last_title_first("GASH joanna"))
   end
   
   def test_comma
-    name = Name.last_title_first("Gash, Joanna")
-    assert_equal("Gash", name.last)
-    assert_equal("Joanna", name.first)
+    assert_equal(@joanna_gash, Name.last_title_first("Gash, Joanna"))
   end
 end
