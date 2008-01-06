02e38545bba1fbc021d579aa23d0e9a9c0c92292
mlandauer <devnull@localhost>
2008-01-06 12:28:34 +1100

    [svn] Can now run this test from any directory

--- tests/test_member_parser.rb
+++ tests/test_member_parser.rb
@@ -1,55 +1,46 @@
 #!/usr/bin/env ruby
 
+$:.unshift "#{File.dirname(__FILE__)}/.."
+
 require 'member-parser'
 require 'test/unit'
 
 class TestMemberParser < Test::Unit::TestCase
-    # mp-13329.html
-    # mp-13291.html
-    # mp-11881.html
-    # mp-11838.html
-    # mp-12065.html
-    #
-    
     def setup
       @url = URI.parse("http://foo.co.uk")
+      @source = File.join(File.dirname(__FILE__), "source")
     end
     
     def test_petro_georgiou
-      doc = Hpricot(open("tests/source/Petro_Georgiou.html"))
-      member = MemberParser::parse(@url, doc)
+      member = read_member("Petro_Georgiou.html")
 
       assert_equal("1994-11-19", member.fromdate)
       assert_equal("by_election", member.fromwhy)        
     end
     
     def test_annette_ellis
-      doc = Hpricot(open("tests/source/Annette_Ellis.html"))
-      member = MemberParser::parse(@url, doc)
-
+      member = read_member("Annette_Ellis.html")
+      
       assert_equal("1996-03-02", member.fromdate)
       assert_equal("general_election", member.fromwhy)        
     end
     
     def test_from_date_simple
-        doc = Hpricot(open("tests/source/Kate_Ellis.html"))
-        member = MemberParser::parse(@url, doc)
+        member = read_member("Kate_Ellis.html")
         
         assert_equal("2004-10-07", member.fromdate)
         assert_equal("general_election", member.fromwhy)        
     end
     
     def test_from_date_by_election
-        doc = Hpricot(open("tests/source/mp-13291.html"))
-        member = MemberParser::parse(@url, doc)
+        member = read_member("mp-13291.html")
         
         assert_equal("1986-02-08", member.fromdate)
         assert_equal("by_election", member.fromwhy)
         
         # Looks like a normal mp not sure 
         # why I selected it for tests
-        doc = Hpricot(open("tests/source/mp-11838.html"))
-        member = MemberParser::parse(@url, doc)
+        member = read_member("mp-11838.html")
         
         assert_equal("2001-07-14", member.fromdate)
         assert_equal("by_election", member.fromwhy)
@@ -57,16 +48,14 @@ class TestMemberParser < Test::Unit::TestCase
     
     def test_from_date_multi_line
         # multiple lines state and fed only interested in fed
-        doc = Hpricot(open("tests/source/mp-12065.html"))
-        member = MemberParser::parse(@url, doc)
+        member = read_member("mp-12065.html")
         
         assert_equal("1994-03-26", member.fromdate)
         assert_equal("by_election", member.fromwhy)
         
         # multiple lines, 2nd line relected dates
         # contiguous service.
-        doc = Hpricot(open("tests/source/mp-13291.html"))
-        member = MemberParser::parse(@url, doc)
+        member = read_member("mp-13291.html")
         
         assert_equal("1986-02-08", member.fromdate)
         assert_equal("by_election", member.fromwhy)
@@ -75,11 +64,16 @@ class TestMemberParser < Test::Unit::TestCase
     def test_from_date_non_contiguous
         # generl election 1993, defeated 1996, general election 1996
         # non contiguous service.
-        doc = Hpricot(open("tests/source/mp-11881.html"))
-        member = MemberParser::parse(@url, doc)
+        member = read_member("mp-11881.html")
         
         assert_equal("1998-10-03", member.fromdate)
         assert_equal("general_election", member.fromwhy)
     end
     
+    # Helper
+    
+    def read_member(filename)
+      doc = Hpricot(open(File.join(@source, filename)))
+      MemberParser::parse(@url, doc)
+    end
 end
