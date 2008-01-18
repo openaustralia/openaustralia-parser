2eb65b61727a1217f96af40f6c97b4c7330bf579
mlandauer <devnull@localhost>
2008-01-18 13:40:06 +1100

    [svn] Gave tests more descriptive names and removed redundant tests

--- tests/test_member_parser.rb
+++ tests/test_member_parser.rb
@@ -25,7 +25,15 @@ class TestMemberParser < Test::Unit::TestCase
     #  assert_equal("by_election", fromwhy)
     #end
     
-    def test_petro_georgiou
+    def test_elected_at_by_election
+      fromdate, fromwhy = MemberParser.parse_parliamentary_service_text(
+        "Elected to the House of Representatives for Aston, Victoria at by-election 14.7.2001 " +
+        "vice PE Nugent (deceased). Re-elected 2001 and 2004.")
+      assert_equal("2001-07-14", fromdate)
+      assert_equal("by_election", fromwhy)
+    end
+    
+    def test_elected_at_by_election_on
       fromdate, fromwhy = MemberParser.parse_parliamentary_service_text(
         "elected to the house of representatives for kooyong, victoria, at by-election on 19.11.1994, " +
         "vice the hon. as peacock (resigned). re-elected 1996, 1998, 2001, 2004 and 2007.")
@@ -33,7 +41,19 @@ class TestMemberParser < Test::Unit::TestCase
       assert_equal("by_election", fromwhy)        
     end
     
-    def test_annette_ellis
+    # This example has parliamentary service text for both the Senate and the House of Representative
+    def test_member_in_senate_and_house_of_representatives
+      fromdate, fromwhy = MemberParser.parse_parliamentary_service_text(
+        "Elected to the Senate for New South Wales 1987 (term deemed to have begun 1.7.1987) " +
+        "and 1990. Resigned 24.2.1994. Elected to the House of Representatives for Mackellar, " +
+        "New South Wales, at by-election 26.3.94 vice the Hon. JJ Carlton (resigned). Re-elected " +
+        "1996, 1998, 2001 and 2004.")
+      assert_equal("1994-03-26", fromdate)
+      assert_equal("by_election", fromwhy)
+    end
+    
+    # Elected at general election
+    def test_elected_at_general_election
       fromdate, fromwhy = MemberParser.parse_parliamentary_service_text(
         "australian capital territory: elected to the australian capital territory legislative assembly " +
         "15.2.1992. defeated 18.2.1995. federal: elected to the house of representatives for namadgi, " +
@@ -43,44 +63,9 @@ class TestMemberParser < Test::Unit::TestCase
       assert_equal("general_election", fromwhy)        
     end
     
-    def test_from_date_by_election
-      fromdate, fromwhy = MemberParser.parse_parliamentary_service_text(
-        "Elected to the House of Representatives for Scullin, Victoria, at by-election 8.2.1986, " +
-        "vice the Hon. Dr HA Jenkins (resigned). Re-elected 1987, 1990, 1993, 1996, 1998, 2001 and 2004.")
-      assert_equal("1986-02-08", fromdate)
-      assert_equal("by_election", fromwhy)
-        
-      # Looks like a normal mp not sure 
-      # why I selected it for tests
-      fromdate, fromwhy = MemberParser.parse_parliamentary_service_text(
-        "Elected to the House of Representatives for Aston, Victoria at by-election 14.7.2001 " +
-        "vice PE Nugent (deceased). Re-elected 2001 and 2004.")
-      assert_equal("2001-07-14", fromdate)
-      assert_equal("by_election", fromwhy)
-    end
-    
-    def test_from_date_multi_line
-      # multiple lines state and fed only interested in fed
-      fromdate, fromwhy = MemberParser.parse_parliamentary_service_text(
-        "Elected to the Senate for New South Wales 1987 (term deemed to have begun 1.7.1987) " +
-        "and 1990. Resigned 24.2.1994. Elected to the House of Representatives for Mackellar, " +
-        "New South Wales, at by-election 26.3.94 vice the Hon. JJ Carlton (resigned). Re-elected " +
-        "1996, 1998, 2001 and 2004.")
-      assert_equal("1994-03-26", fromdate)
-      assert_equal("by_election", fromwhy)
-        
-      # multiple lines, 2nd line relected dates
-      # contiguous service.
-      fromdate, fromwhy = MemberParser.parse_parliamentary_service_text(
-        "Elected to the House of Representatives for Scullin, Victoria, at by-election 8.2.1986, " +
-        "vice the Hon. Dr HA Jenkins (resigned). Re-elected 1987, 1990, 1993, 1996, 1998, 2001 and 2004.")
-      assert_equal("1986-02-08", fromdate)
-      assert_equal("by_election", fromwhy)
-    end
-    
-    def test_from_date_non_contiguous
-      # generl election 1993, defeated 1996, general election 1996
-      # non contiguous service.
+    # General election 1993, defeated 1996, general election 1996
+    # Non contiguous service.
+    def test_non_contiguous_service
       fromdate, fromwhy = MemberParser.parse_parliamentary_service_text(
         "Elected to the House of Representatives for Lilley, Queensland, 1993. Defeated at general " +
         "elections 1996. Re-elected 1998, 2001 and 2004.")
