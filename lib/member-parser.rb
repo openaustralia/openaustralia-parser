9a8c24924ec3d6b859053cdc8ced84f844436173
Matthew Landauer <matthew@openaustralia.org>
2008-02-09 12:15:16 +1100

    Extracted methods

--- lib/member-parser.rb
+++ lib/member-parser.rb
@@ -31,10 +31,7 @@ class MemberParser
       m[1] if m
     end
     
-    def self.parse_house_service_current_member(text)
-      # We're assuming that the member is continuously in parliament
-      # TODO: Handle member losing election and being re-elected
-      
+    def self.parse_start_from_house_service(text)
       if text =~ /by-election/i
           m = text.match(/elected to the house of representatives for.*by-election( on)? ([.0-9]*)/i)
           to_format = m[2]
@@ -45,45 +42,39 @@ class MemberParser
           
           n_date = Date.new(year, d[2].to_i, d[1].to_i)
           from_date = n_date.to_s
-          fromwhy = "by_election"
+          from_why = "by_election"
       else
           m = text.match(/elected to the house of representatives for[^0-9]*([0-9]*)/i)
           from_date = @election_dates[m[1]]
-          fromwhy = "general_election"
+          from_why = "general_election"
       end
 
-      to_date = "9999-12-31"
-      to_why = "still_in_office"
-      
-      return from_date, fromwhy, to_date, to_why
+      return from_date, from_why
     end
     
     def self.parse_house_service_former_member(text)
       # We're assuming that the member is continuously in parliament
       # TODO: Handle member losing election and being re-elected
-      
-      if text =~ /by-election/i
-          m = text.match(/elected to the house of representatives for.*by-election( on)? ([.0-9]*)/i)
-          to_format = m[2]
-          d = to_format.match(/([0-9]*).([0-9]*).([0-9]*)/)
-          
-          year = d[3].to_i
-          year += 1900 if year < 1900 
-          
-          n_date = Date.new(year, d[2].to_i, d[1].to_i)
-          from_date = n_date.to_s
-          fromwhy = "by_election"
-      else
-          m = text.match(/elected to the house of representatives for[^0-9]*([0-9]*)/i)
-          from_date = @election_dates[m[1]]
-          fromwhy = "general_election"
-      end
+
+      from_date, from_why = parse_start_from_house_service(text)
 
       # TODO: This is wrong!
       to_date = "9999-12-31"
       to_why = "still_in_office"
       
-      return from_date, fromwhy, to_date, to_why
+      return from_date, from_why, to_date, to_why
+    end
+    
+    def self.parse_house_service_current_member(text)
+      # We're assuming that the member is continuously in parliament
+      # TODO: Handle member losing election and being re-elected
+      
+      from_date, from_why = parse_start_from_house_service(text)
+
+      to_date = "9999-12-31"
+      to_why = "still_in_office"
+      
+      return from_date, from_why, to_date, to_why
     end
     
     def self.parse_party(party)
@@ -121,84 +112,59 @@ class MemberParser
     # parses member information from http://parlinfoweb.aph.gov.au/
     # expects the url of the page and an html dom for hpricot
     # returns a Member
-    def self.parse_current_member(url, doc)
-        name = Name.last_title_first(doc.search("#txtTitle").inner_text.to_s[14..-1])
-        constituency = doc.search("#dlMetadata__ctl3_Label3").inner_html
-        content = doc.search('div#contentstart')
-        party = parse_party(content.search("p")[1].inner_html)
-
-        # Grab image of member
-        img_tag = content.search("img").first
-        # If image is available
-        if img_tag
-          relative_image_url = img_tag.attributes['src']
-          image_url = url + URI.parse(relative_image_url)
-        end
-
-        # Collect up all the text between the <h2>Parliamentary service</h2> and the next <h2> tag
-        psText = doc.to_html.match(/<h2>parliamentary service<\/h2>(.*?)<h2>/mi)[1]
-        # Need to remove all tags and replace with space
-        psText.gsub!(/<\/?[^>]*>/, " ")
-        house_service = extract_house_service_from_parliamentary_service(psText)
+    def self.parse_member(url, doc, current_member)
+      name = Name.last_title_first(doc.search("#txtTitle").inner_text.to_s[14..-1])
+      constituency = doc.search("#dlMetadata__ctl3_Label3").inner_html
+      content = doc.search('div#contentstart')
+      party = parse_party(content.search("p")[1].inner_html)
+      
+      # Grab image of member
+      img_tag = content.search("img").first
+      # If image is available
+      if img_tag
+        relative_image_url = img_tag.attributes['src']
+        image_url = url + URI.parse(relative_image_url)
+      end
+      
+      # Collect up all the text between the <h2>Parliamentary service</h2> and the next <h2> tag
+      match = doc.to_html.match(/<h2>parliamentary service<\/h2>(.*?)<h2>/mi)
+      # Barry Thomas Cunningham's biography page is all messed up. This should skip it
+      # TODO: Handle Barry Thomas Cunningham correctly (with the correct info returned rather than just skipping)
+      return nil if match.nil?
+      psText = match[1]
+      # Need to remove all tags and replace with space
+      psText.gsub!(/<\/?[^>]*>/, " ")
+      house_service = extract_house_service_from_parliamentary_service(psText)
+      # If this person hasn't been a member of the house of representatives then ignore
+      return nil if house_service.nil?
+      if current_member
         from_date, from_why, to_date, to_why = parse_house_service_current_member(house_service)
-
+      else
+        from_date, from_why, to_date, to_why = parse_house_service_former_member(house_service)
+      end
+      
       member = Member.new(:id_member => @@id_member, :id_person => @@id_person,
-            :house => "commons",
-            :name => name,
-            :constituency => constituency,
-            :party => party,
-            :fromdate => from_date,
-            :todate => to_date,
-            :fromwhy => from_why,
-            :towhy => to_why,
-            :image_url => image_url)
-
-        @@id_member = @@id_member + 1
-        @@id_person = @@id_person + 1
-
-        member
+        :house => "commons",
+        :name => name,
+        :constituency => constituency,
+        :party => party,
+        :fromdate => from_date,
+        :todate => to_date,
+        :fromwhy => from_why,
+        :towhy => to_why,
+        :image_url => image_url)
+      
+      @@id_member = @@id_member + 1
+      @@id_person = @@id_person + 1
+      
+      member
     end
     
     def self.parse_former_member(url, doc)
-        name = Name.last_title_first(doc.search("#txtTitle").inner_text.to_s[14..-1])
-        constituency = doc.search("#dlMetadata__ctl3_Label3").inner_html
-        content = doc.search('div#contentstart')
-        party = parse_party(content.search("p")[1].inner_html)
-
-        # Grab image of member
-        img_tag = content.search("img").first
-        # If image is available
-        if img_tag
-          relative_image_url = img_tag.attributes['src']
-          image_url = url + URI.parse(relative_image_url)
-        end
-
-        # Collect up all the text between the <h2>Parliamentary service</h2> and the next <h2> tag
-        match = doc.to_html.match(/<h2>parliamentary service<\/h2>(.*?)<h2>/mi)
-        # Barry Thomas Cunningham's biography page is all messed up. This should skip it
-        return nil if match.nil?
-        psText = match[1]
-        # Need to remove all tags and replace with space
-        psText.gsub!(/<\/?[^>]*>/, " ")
-        house_service = extract_house_service_from_parliamentary_service(psText)
-        return nil if house_service.nil?
-
-      from_date, from_why, to_date, to_why = parse_house_service_former_member(house_service)
-
-      member = Member.new(:id_member => @@id_member, :id_person => @@id_person,
-            :house => "commons",
-            :name => name,
-            :constituency => constituency,
-            :party => party,
-            :fromdate => from_date,
-            :todate => to_date,
-            :fromwhy => from_why,
-            :towhy => to_why,
-            :image_url => image_url)
-
-        @@id_member = @@id_member + 1
-        @@id_person = @@id_person + 1
-
-        member
+      parse_member(url, doc, false)
     end
+
+    def self.parse_current_member(url, doc)
+      parse_member(url, doc, true)
+    end    
 end
