b1ab0b46a704f6b6858ceef44f026363352902a7
Matthew Landauer <matthew@openaustralia.org>
2008-02-22 17:28:27 +1100

    Simplified the way rcov is run

--- rakefile.rb
+++ rakefile.rb
@@ -15,9 +15,7 @@ namespace :test do
   desc 'Measures test coverage'
   task :coverage do
     rm_f "coverage"
-    rm_f "coverage.data"
-    rcov = 'rcov --aggregate coverage.data --text-summary -Ilib -x/Library/'
-    system("#{rcov} --html tests/test_*.rb")
+    system("rcov --text-summary -Ilib -x/Library/ tests/test_*.rb")
     system("open coverage/index.html") if PLATFORM['darwin']
   end
 
