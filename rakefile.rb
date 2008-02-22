c157dab2102b28dfb577bb3fce1a4273785a243e
Matthew Landauer <matthew@openaustralia.org>
2008-02-22 17:02:42 +1100

    Added code coverage testing

--- rakefile.rb
+++ rakefile.rb
@@ -9,3 +9,16 @@ Rake::TestTask.new do |t|
     t.test_files = FileList['tests/test*.rb']
     t.verbose = true
 end
+
+namespace :test do
+
+  desc 'Measures test coverage'
+  task :coverage do
+    rm_f "coverage"
+    rm_f "coverage.data"
+    rcov = 'rcov --aggregate coverage.data --text-summary -Ilib -x/Library/'
+    system("#{rcov} --html tests/test_*.rb")
+    system("open coverage/index.html") if PLATFORM['darwin']
+  end
+
+end
