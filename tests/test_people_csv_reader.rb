bc38a723ca386975e6324d5c63bf2a84bcf95088
Matthew Landauer <matthew@openaustralia.org>
2008-02-24 15:18:30 +1100

    Oops. Updating the csv file to include the senators as well changed the order of things slightly so the fragile test (which included id's) broke. Fixed now.

--- tests/test_people_csv_reader.rb
+++ tests/test_people_csv_reader.rb
@@ -15,11 +15,11 @@ class TestPeopleCSVReader < Test::Unit::TestCase
     people = PeopleCSVReader.read("#{File.dirname(__FILE__)}/../data/members.csv")
     sophie_mirabella = people.find_person_by_name(Name.new(:first => "Sophie", :last => "Mirabella"))
     
-    ref = Person.new(10319)
+    ref = Person.new(10317)
     ref.add_house_period(:name => Name.new(:first => "Sophie", :last => "Mirabella"),
       :from_date => Date.new(2001, 11, 10), :to_date => Date.new(9999, 12, 31),
       :from_why => "general_election", :to_why => "current_member",
-      :division => "Indi", :party => "LIB", :id => 376)
+      :division => "Indi", :party => "LIB", :id => 383)
 
     assert_equal(ref, sophie_mirabella)
   end
