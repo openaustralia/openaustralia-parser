458b0a8e0059c71e62cce2afcba80dea2ac2a480
Matthew Landauer <matthew@openaustralia.org>
2008-03-28 21:53:21 +1100

    Puts xml directly into openaustralia-app directory rather than staging in the parser area first.

--- parse-members.rb
+++ parse-members.rb
@@ -5,16 +5,26 @@ $:.unshift "#{File.dirname(__FILE__)}/lib"
 require 'configuration'
 require 'people'
 
+conf = Configuration.new
+
+system("mkdir -p #{conf.web_root}/pwdata/members")
+
+# Copy across files that are needed for the script xml2db to run but are not yet populated with data
+["bbc-links.xml", "constituencies.xml", "diocese-bishops.xml", "edm-links.xml", "expenses200102.xml",
+  "expenses200203.xml", "expenses200304.xml", "expenses200405.xml", "expenses200506.xml", "expenses200506former.xml",
+  "expenses200607.xml", "guardian-links.xml", "journa-list.xml", "lordbiogs.xml", "ni-members.xml", "peers-ucl.xml",
+  "royals.xml", "sp-members.xml", "websites.xml", "wikipedia-commons.xml", "wikipedia-lords.xml", "wikipedia-mla.xml",
+  "wikipedia-msp.xml"].each do |file|
+    system("cp data/empty-template.xml #{conf.web_root}/pwdata/members/#{file}")
+end
+
 puts "Writing XML..."
 people = People.read_csv("data/members.csv", "data/ministers.csv")
-people.write_xml('pwdata/members/people.xml', 'pwdata/members/all-members.xml', 'pwdata/members/ministers.xml')
+people.write_xml("#{conf.web_root}/pwdata/members/people.xml", "#{conf.web_root}/pwdata/members/all-members.xml",
+  "#{conf.web_root}/pwdata/members/ministers.xml")
 
 puts "Downloading person images..."
-people.download_images("pwdata/images/mps", "pwdata/images/mpsL")
+people.download_images("#{conf.web_root}/twfy/www/docs/images/mps", "#{conf.web_root}/twfy/www/docs/images/mpsL")
 
 # And load up the database
-conf = Configuration.new
-system(conf.web_root + "/twfy/scripts/xml2db.pl --members --all --force")
-image_dir = conf.web_root + "/twfy/www/docs/images"
-system("rm -rf " + image_dir + "/mps/*.jpg " + image_dir + "/mpsL/*.jpg")
-system("cp -R pwdata/images/* " + image_dir)
+system("#{conf.web_root}/twfy/scripts/xml2db.pl --members --all --force")
--- parse-speeches.rb
+++ parse-speeches.rb
@@ -6,20 +6,24 @@ require 'people'
 require 'hansard_parser'
 require 'configuration'
 
+conf = Configuration.new
+
+system("mkdir -p #{conf.web_root}/pwdata/scrapedxml/debates")
+system("mkdir -p #{conf.web_root}/pwdata/scrapedxml/regmem")
+
+# Copy across file that is needed for the script xml2db to run but is not yet populated with data
+system("cp data/empty-template.xml #{conf.web_root}/pwdata/scrapedxml/regmem/regmem2000-01-01.xml")
+
 # First load people back in so that we can look up member id's
 people = People.read_csv("data/members.csv", "data/ministers.csv")
 
-system("mkdir -p pwdata/scrapedxml/debates")
-
 date = Date.new(2007, 9, 20)
-xml_filename = "pwdata/scrapedxml/debates/debates#{date}.xml"
+xml_filename = "#{conf.web_root}/pwdata/scrapedxml/debates/debates#{date}.xml"
 
 HansardParser.parse_date(date, xml_filename, people)
 
 # Temporary hack: nicely indent XML
 system("tidy -quiet -indent -xml -modify -wrap 0 -utf8 #{xml_filename}")
-
-conf = Configuration.new
 
 # And load up the database
 system(conf.web_root + "/twfy/scripts/xml2db.pl --debates --all --force")
