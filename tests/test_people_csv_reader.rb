3cec3e5e10fe480014091a0bfc570ee13a39900b
Matthew Landauer <matthew@openaustralia.org>
2008-04-06 14:20:28 +1000

    Fixed up unit test to go along with recent changes

--- tests/test_people_csv_reader.rb
+++ tests/test_people_csv_reader.rb
@@ -18,8 +18,8 @@ class TestPeopleCSVReader < Test::Unit::TestCase
     
     ref = Person.new(Name.new(:first => "Sophie", :last => "Mirabella"), Id.new("uk.org.publicwhip/person/", 10461))
     ref.add_house_period(:from_date => Date.new(2001, 11, 10), :to_date => DateWithFuture.future,
-      :from_why => "general_election", :to_why => "current_member",
-      :division => "Indi", :party => "LIB", :id => Id.new("uk.org.publicwhip/member/", 376))
+      :from_why => "general_election", :to_why => "still_in_office",
+      :division => "Indi", :party => "Liberal Party", :id => Id.new("uk.org.publicwhip/member/", 378))
 
     assert_equal(ref, sophie_mirabella)
   end
