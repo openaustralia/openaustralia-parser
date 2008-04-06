c2e4eb0d38e3811410e461f9551a418eb8412f72
Matthew Landauer <matthew@openaustralia.org>
2008-04-06 14:20:03 +1000

    test_people can now be run from any directory

--- tests/test_people.rb
+++ tests/test_people.rb
@@ -6,7 +6,7 @@ require 'people'
 
 class TestPeople < Test::Unit::TestCase
   def test_speaker
-    people = People.read_csv("../data/members.csv", "../data/ministers.csv")
+    people = People.read_csv("#{File.dirname(__FILE__)}/../data/members.csv", "#{File.dirname(__FILE__)}/../data/ministers.csv")
     assert_equal("David Peter Maxwell Hawker", people.house_speaker(Date.new(2007,10,1)).person.name.full_name)
   end
 end
