01049edaf77b074fd20fb166e4fb838f736c9027
Matthew Landauer <matthew@openaustralia.org>
2008-04-11 15:51:43 +1000

    Reordered methods so that they are grouped by whether they return periods or people

--- lib/people.rb
+++ lib/people.rb
@@ -14,7 +14,32 @@ class People < Array
     super
   end
   
-  # Returns member
+  # Methods that return Person object
+  
+  # Returns nil if non found. Throws exception if more than one match
+  def find_person_by_name(name)
+    matches = find_people_by_name(name)
+    throw "More than one match for name #{name.full_name} found" if matches.size > 1
+    matches[0] if matches.size == 1
+  end
+  
+  def find_person_by_name_current_on_date(name, date)
+    matches = find_people_by_name_current_on_date(name, date)
+    throw "More than one match for name #{name.full_name} found" if matches.size > 1
+    matches[0] if matches.size == 1
+  end
+  
+  # Returns all the people that match a particular name and have current senate/house of representatives positions on the date
+  def find_people_by_name_current_on_date(name, date)
+    find_people_by_name(name).find_all {|p| p.current_position_on_date?(date)} 
+  end
+  
+  def find_people_by_name(name)
+    find_all{|p| name.matches?(p.name)}
+  end    
+  
+  # Methods that return Period objects
+  
   def house_speaker(date)
     # HACK
     if date < Date.new(2008, 2, 12)
@@ -38,20 +63,7 @@ class People < Array
     throw "Couldn't find deputy speaker #{name.full_name}" if member.nil?
     member
   end
-  
-  # Returns nil if non found. Throws exception if more than one match
-  def find_person_by_name(name)
-    matches = find_people_by_name(name)
-    throw "More than one match for name #{name.full_name} found" if matches.size > 1
-    matches[0] if matches.size == 1
-  end
-  
-  def find_person_by_name_current_on_date(name, date)
-    matches = find_people_by_name_current_on_date(name, date)
-    throw "More than one match for name #{name.full_name} found" if matches.size > 1
-    matches[0] if matches.size == 1
-  end
-  
+
   def find_member_by_name_current_on_date(name, date)
     matches = find_members_by_name_current_on_date(name, date)
     throw "More than one match for name #{name.full_name} found" if matches.size > 1
@@ -62,15 +74,6 @@ class People < Array
     find_house_members_current_on(date).find_all {|m| name.matches?(m.person.name)}
   end
   
-  # Returns all the people that match a particular name and have current senate/house of representatives positions on the date
-  def find_people_by_name_current_on_date(name, date)
-    find_people_by_name(name).find_all {|p| p.current_position_on_date?(date)} 
-  end
-  
-  def find_people_by_name(name)
-    find_all{|p| name.matches?(p.name)}
-  end    
-  
   # Returns the house members that are members on the given date
   def find_house_members_current_on(date)
     all_house_periods.find_all {|m| m.current_on_date?(date)}
@@ -79,6 +82,8 @@ class People < Array
   def find_house_period_by_id(id)
     all_house_periods.find{|p| p.id == id}
   end
+  
+  # End of methods that return Period objects
   
   # Facade for readers and writers
   def People.read_csv(members_filename, ministers_filename)
