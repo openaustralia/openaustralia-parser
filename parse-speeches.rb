aa1efdc9a06fdf9944e137229e7f2ba12b25f6c4
Matthew Landauer <matthew@openaustralia.org>
2008-04-29 18:12:41 +1000

    Removed comment about problem page (fixed in last commit)

--- parse-speeches.rb
+++ parse-speeches.rb
@@ -23,7 +23,6 @@ people = People.read_csv("data/members.csv", "data/ministers.csv")
 #
 # Problem dates:
 # 2007.6.18: President speaks: 2007.6.18
-# 2007.6.21: Multiple matches for name Hon. BK Bishop found
 # 2007.9.11: No match for name Rt Hon. STEPHEN HARPER found
 #
 #from_date = Date.new(2008, 3, 28)
