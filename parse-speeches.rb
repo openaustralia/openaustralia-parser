f94b909e5d9ef6c49305a573d7760d5d255bb236
Matthew Landauer <matthew@openaustralia.org>
2008-05-11 17:55:31 +1000

    Added command-line parsing to parse-speeches

--- parse-speeches.rb
+++ parse-speeches.rb
@@ -5,6 +5,49 @@ $:.unshift "#{File.dirname(__FILE__)}/lib"
 require 'people'
 require 'hansard_parser'
 require 'configuration'
+require 'optparse'
+
+def parse_date(text)
+  if text == "today"
+    Date.today
+  else
+    Date.parse(text)
+  end
+end
+
+# Defaults
+options = {:load_database => true}
+
+OptionParser.new do |opts|
+  opts.banner = <<EOF
+Usage: parse-speeches.rb [options] <from-date> [<to-date>]
+    In <from-date> and <to-date> can use "today" for today's date
+    
+    Interesting dates:
+      Last day of 2007 parliament: 2007.9.20
+      First day of 2008 parliament: 2008.2.12
+    Problem dates:
+      2007.6.18: President speaks: 2007.6.18
+      2007.9.11: No match for name Rt Hon. STEPHEN HARPER found
+
+EOF
+  opts.on("--no-load", "Just generate XML and don't load up database") do |l|
+    options[:load_database] = l
+  end
+end.parse!
+
+if ARGV.size != 1 && ARGV.size != 2
+  puts "Need to supply one or two dates"
+  exit
+end
+    
+from_date = parse_date(ARGV[0])
+
+if ARGV.size == 1
+  to_date = from_date
+else
+  to_date = parse_date(ARGV[1])
+end
 
 conf = Configuration.new
 
@@ -17,19 +60,6 @@ system("cp data/empty-template.xml #{conf.xml_path}/scrapedxml/regmem/regmem2000
 # First load people back in so that we can look up member id's
 people = People.read_csv("data/members.csv", "data/ministers.csv")
 
-# Interesting dates:
-# Last day of 2007 parliament: 2007.9.20)
-# First day of 2008 parliament: 2008.2.12
-#
-# Problem dates:
-# 2007.6.18: President speaks: 2007.6.18
-# 2007.9.11: No match for name Rt Hon. STEPHEN HARPER found
-#
-#from_date = Date.new(2008, 3, 28)
-#to_date = Date.today
-from_date = Date.new(2008, 2, 12)
-to_date = Date.new(2008, 2, 12)
-
 parser = HansardParser.new(people)
 
 date = from_date
@@ -39,4 +69,4 @@ while date <= to_date
 end
 
 # And load up the database
-system(conf.web_root + "/twfy/scripts/xml2db.pl --debates --all --force")
+system(conf.web_root + "/twfy/scripts/xml2db.pl --debates --all --force") if options[:load_database]
