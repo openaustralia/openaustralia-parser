58719a29715e9bdf38ef41a5e0722a41f60cf3d8
Matthew Landauer <matthew@openaustralia.org>
2008-05-25 15:59:03 +1000

    Speeches class is now replaced by Debates which holds both speeches and headings

--- lib/speech.rb
+++ lib/speech.rb
@@ -1,22 +1,26 @@
 class Speech
   attr_accessor :speaker, :time, :url, :id, :content
   
-  def initialize(speaker = nil, time = nil, url = nil, id = nil, content = Hpricot::Elements.new)
+  def initialize(speaker, time, url, count, date)
     @speaker = speaker
     @time = time
     @url = url
-    @id = id
-    @content = content
+    @count = count
+    @date = date
+    @content = Hpricot::Elements.new
   end
   
+  def id
+    "uk.org.publicwhip/debate/#{@date}.#{@count}"
+  end
+
   def output(x)
     if @speaker
-      x.speech(:speakername => @speaker.name.full_name, :time => @time, :url => @url, :id => @id,
+      x.speech(:speakername => @speaker.name.full_name, :time => @time, :url => @url, :id => id,
         :speakerid => @speaker.id) { x << @content.to_s }
     else
-      x.speech(:speakername => "unknown", :time => @time, :url => @url, :id => @id) { x << @content.to_s }
+      x.speech(:speakername => "unknown", :time => @time, :url => @url, :id => id) { x << @content.to_s }
     end
-    @id.next
   end
 
   def append_to_content(content)
