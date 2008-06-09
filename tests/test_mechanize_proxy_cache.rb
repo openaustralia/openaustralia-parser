e6b8193f333ff96311a464176e54cb3cb250afc2
Matthew Landauer <matthew@openaustralia.org>
2008-06-09 13:44:52 +1000

    Modified test to show up what happens with "&amp;"

--- tests/test_mechanize_proxy_cache.rb
+++ tests/test_mechanize_proxy_cache.rb
@@ -4,14 +4,18 @@ require 'test/unit'
 require 'mechanize_proxy'
 
 class TestMechanizeProxyCache < Test::Unit::TestCase
+  def setup
+    @cache = MechanizeProxyCache.new
+    @cache.cache_subdirectory = "test"
+  end
+  
   def test_quoting_on_attributes
-    page = PageProxy.new(Hpricot('<a href="http://www.blah.com?foo&blah">Some Text</a>'), "http://test.com/test1.html") 
+    page = PageProxy.new(Hpricot('<a href="http://www.blah.com?foo&amp;blah">Some Text</a>'), "http://test.com/test1.html") 
     assert_equal('http://www.blah.com?foo&blah', page.parser.search('a').first.get_attribute('href'))
-    cache = MechanizeProxyCache.new
-    cache.write_cache(page)
+    @cache.write_cache(page)
     
     # Reload from cache
-    page2 = cache.read_cache(page.uri)
+    page2 = @cache.read_cache(page.uri)
     assert_equal('http://www.blah.com?foo&blah', page2.parser.search('a').first.get_attribute('href'))
   end
 end
