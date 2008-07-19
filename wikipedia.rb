d7eaf51ee198647cd6b1c6cd504267a7596af03a
Matthew Landauer <matthew@openaustralia.org>
2008-07-19 10:56:34 +1000

    Extracting correct column in table via a slightly simpler way; Extracted function.

--- wikipedia.rb
+++ wikipedia.rb
@@ -1,75 +1,45 @@
 #!/usr/bin/env ruby
+# Figures out the URLs for the Wikipedia biography pages of Representatives and Senators
 
 $:.unshift "#{File.dirname(__FILE__)}/lib"
 
-require 'mechanize_proxy'
 require 'name'
 require 'people'
-require 'configuration'
 require 'hpricot'
 require 'open-uri'
+require 'configuration'
+
+def extract_links_from_wikipedia(doc, filename, people)
+  xml = File.open(filename, 'w')
+  x = Builder::XmlMarkup.new(:target => xml, :indent => 1)
+  x.instruct!  
+  x.publicwhip do
+    doc.search("//table[@class='wikitable sortable']").search("tr").each do |row|
+      link = row.search('td a')[0]
+      if link
+        name = Name.title_first_last(link.inner_html)
+        person = people.find_person_by_name_current_on_date(name, Date.today)
+        if person
+          x.personinfo(:id => person.id, :wikipedia_url => "http://en.wikipedia.org#{link.get_attribute("href")}")
+        else
+          puts "WARNING: Could not find person with name #{name.full_name}" 
+        end 
+      end
+    end
+  end
+  xml.close
+end
 
 conf = Configuration.new
-
-agent = MechanizeProxy.new
-agent.cache_subdirectory = "parse-wikipedia"
 
 puts "Reading member data..."
 people = people = PeopleCSVReader.read_members
 
-puts "Wikipedia links for MPs ..."
-
-doc = Hpricot(open("http://en.wikipedia.org/wiki/Members_of_the_Australian_House_of_Representatives%2C_2007-2010"))
-doc_sen = Hpricot(open("http://en.wikipedia.org/wiki/Members_of_the_Australian_Senate%2C_2005-2008"))
-
-xml_mps = File.open("#{conf.members_xml_path}/wikipedia-commons.xml", 'w')
-xml_sen = File.open("#{conf.members_xml_path}/wikipedia-lords.xml", 'w')
-x = Builder::XmlMarkup.new(:target => xml_mps, :indent => 1)
-x.instruct!  
-x.publicwhip do
-  # House of Representatives
-  doc.search("//table[@class='wikitable sortable']").search("td").each do |cell|
-    cell.search("a").each do |link|
-      nametext = link.inner_html	
-      url = link.get_attribute("href")
-      if nametext =~ /\d{4}/ or nametext =~ /.*(Australia|Party|National|Queensland|Liberal|Tasmania|First|Wales|Victoria|Independent)/ or
-        nametext.length < 6 or url =~ /^\/wiki\/Division_of.*/
-        # do nothing
-      else
-        person = people.find_person_by_name_current_on_date(Name.title_first_last(nametext), Date.today)
-        if person
-          wiki_url = "http://en.wikipedia.org#{url}"
-          params = {:id => person.id, :wikipedia_url => wiki_url}
-          x.personinfo(params)
-        else
-          puts "WARNING: Could not find MP with name #{nametext}" 
-        end 
-      end
-    end
-  end
-end
-
-puts "Wikipedia links for senators ..."
-xsen = Builder::XmlMarkup.new(:target => xml_sen, :indent => 1)
-xsen.instruct!
-xsen.publicwhip do
-  doc_sen.search("//table[@class='wikitable sortable']").search("td").each do |cell|
-    cell.search("a").each do |link|
-      nametext = link.inner_html	
-      if nametext =~ /\d{4}/ or nametext =~ /.*(Australia|Party|National|Queensland|Liberal|Tasmania|First|Wales|Victoria|Territory|Democrat)/ or nametext.length < 6
-        # do nothing
-      else
-        person = people.find_person_by_name_current_on_date(Name.title_first_last(nametext), Date.today)
-        if person
-          wiki_url = "http://en.wikipedia.org#{link.get_attribute("href")}"
-          params = {:id => person.id, :wikipedia_url => wiki_url}
-          xsen.personinfo(params)
-        else
-          puts "WARNING: Could not find senator with name #{nametext}" 
-        end 
-      end
-    end
-  end
-end
-xml_mps.close
-xml_sen.close
+puts "Wikipedia links for Representatives..."
+extract_links_from_wikipedia(
+  Hpricot(open("http://en.wikipedia.org/wiki/Members_of_the_Australian_House_of_Representatives%2C_2007-2010")),
+  "#{conf.members_xml_path}/wikipedia-commons.xml", people) if conf.write_xml_representatives
+puts "Wikipedia links for Senators..."
+extract_links_from_wikipedia(
+  Hpricot(open("http://en.wikipedia.org/wiki/Members_of_the_Australian_Senate%2C_2005-2008")),
+  "#{conf.members_xml_path}/wikipedia-lords.xml", people) if conf.write_xml_senators
