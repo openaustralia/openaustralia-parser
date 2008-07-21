f0d85835f63504c0b833f149c0dcfc2d25fcc123
Matthew Landauer <matthew@openaustralia.org>
2008-07-21 17:12:58 +1000

    Extracted separate method for writing xml file

--- wikipedia.rb
+++ wikipedia.rb
@@ -9,23 +9,29 @@ require 'hpricot'
 require 'open-uri'
 require 'configuration'
 
-def extract_links_from_wikipedia(doc, filename, people)
+def extract_links_from_wikipedia(doc, people)
+  links = {}
+  doc.search("//table[@class='wikitable sortable']").search("tr").each do |row|
+    link = row.search('td a')[0]
+    if link
+      name = Name.title_first_last(link.inner_html)
+      person = people.find_person_by_name(name)
+      if person
+        links[person.id] = "http://en.wikipedia.org#{link.get_attribute("href")}"
+      else
+        puts "WARNING: Could not find person with name #{name.full_name}" 
+      end 
+    end
+  end
+  links
+end
+
+def write_links(links, filename)
   xml = File.open(filename, 'w')
   x = Builder::XmlMarkup.new(:target => xml, :indent => 1)
   x.instruct!  
   x.publicwhip do
-    doc.search("//table[@class='wikitable sortable']").search("tr").each do |row|
-      link = row.search('td a')[0]
-      if link
-        name = Name.title_first_last(link.inner_html)
-        person = people.find_person_by_name(name)
-        if person
-          x.personinfo(:id => person.id, :wikipedia_url => "http://en.wikipedia.org#{link.get_attribute("href")}")
-        else
-          puts "WARNING: Could not find person with name #{name.full_name}" 
-        end 
-      end
-    end
+    links.each { |link| x.personinfo(:id => link[0], :wikipedia_url => link[1]) }
   end
   xml.close
 end
@@ -35,13 +41,17 @@ conf = Configuration.new
 puts "Reading member data..."
 people = people = PeopleCSVReader.read_members
 
-puts "Wikipedia links for Representatives..."
-extract_links_from_wikipedia(
-  Hpricot(open("http://en.wikipedia.org/wiki/Members_of_the_Australian_House_of_Representatives%2C_2007-2010")),
-  "#{conf.members_xml_path}/wikipedia-commons.xml", people) if conf.write_xml_representatives
-puts "Wikipedia links for Senators..."
-extract_links_from_wikipedia(
-  Hpricot(open("http://en.wikipedia.org/wiki/Members_of_the_Australian_Senate%2C_2005-2008")),
-  "#{conf.members_xml_path}/wikipedia-lords.xml", people) if conf.write_xml_senators
+if conf.write_xml_representatives
+  puts "Wikipedia links for Representatives..."
+  links = extract_links_from_wikipedia(
+    Hpricot(open("http://en.wikipedia.org/wiki/Members_of_the_Australian_House_of_Representatives%2C_2007-2010")), people)
+  write_links(links, "#{conf.members_xml_path}/wikipedia-commons.xml")
+end
+if conf.write_xml_senators
+  puts "Wikipedia links for Senators..."
+  links = extract_links_from_wikipedia(
+    Hpricot(open("http://en.wikipedia.org/wiki/Members_of_the_Australian_Senate%2C_2005-2008")), people)
+  write_links(links, "#{conf.members_xml_path}/wikipedia-lords.xml")
+end
 
 system(conf.web_root + "/twfy/scripts/mpinfoin.pl links")
