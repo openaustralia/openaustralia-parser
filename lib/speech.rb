40a96adaa51ff275ebc8b0811dbd0bfe6d1ee754
Matthew Landauer <matthew@openaustralia.org>
2008-08-18 16:24:16 +1000

    Added quoting of URL back in because it turns out if this is not there things appear to work but the HTML is not standards compliant

--- lib/speech.rb
+++ lib/speech.rb
@@ -13,13 +13,18 @@ class Speech
       @logger.error "Empty speech by #{@speaker.person.name.full_name} on #{@sub_page_permanent_url}"
     end
     if @speaker
-      x.speech(:speakername => @speaker.name.full_name, :time => time, :url => @url, :id => id,
+      x.speech(:speakername => @speaker.name.full_name, :time => time, :url => url_quote(@url), :id => id,
         :speakerid => @speaker.id) { x << @content.to_s }
     else
-      x.speech(:speakername => "unknown", :time => time, :url => @url, :id => id) { x << @content.to_s }
+      x.speech(:speakername => "unknown", :time => time, :url => url_quote(@url), :id => id) { x << @content.to_s }
     end
   end
 
+  # Quoting of url's is required to be nice and standards compliant
+  def url_quote(url)
+    url.gsub('&', '&amp;')
+  end
+  
   def append_to_content(content)
     if content.kind_of?(Array)
       @content = @content + content
