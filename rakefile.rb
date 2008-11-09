b82deafa2f039b3e0eb424c02b33ca48eba436d9
Matthew Landauer <matthew@openaustralia.org>
2008-11-10 09:07:09 +1100

    Renamed regression test directory and files to ensure that autotest does not run the regression tests only the unit tests

--- rakefile.rb
+++ rakefile.rb
@@ -5,8 +5,7 @@ require 'rake/rdoctask'
 task :default => [:test]
 
 Rake::TestTask.new do |t|
-    #t.libs << "test"
-    t.test_files = FileList['tests/test*.rb']
+    t.test_files = FileList['test/test*.rb']
     t.verbose = true
 end
 
@@ -15,7 +14,7 @@ namespace :test do
   desc 'Measures test coverage'
   task :coverage do
     rm_f "coverage"
-    system("rcov --text-summary -Ilib -x/Library/ tests/test_*.rb")
+    system("rcov --text-summary -Ilib -x/Library/ test/test_*.rb")
     system("open coverage/index.html") if PLATFORM['darwin']
   end
 
