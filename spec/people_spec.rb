0b8467414989697422a1a238e25276e03df113d9
Matthew Landauer <matthew@openaustralia.org>
2009-04-01 12:50:40 +1100

    Rejigged specs so that they aren't dependent on the actual people data

--- spec/people_spec.rb
+++ spec/people_spec.rb
@@ -7,35 +7,34 @@ require "people"
 
 describe People do
   before :each do
-    # TODO: This is slow
-    @people = PeopleCSVReader.read_members
-    PeopleCSVReader.read_all_ministers(@people)
+    @people = People.new
+
+    @people << Person.new(:name => Name.new(:first => "John", :last => "Smith"), :count => 1)
+    @people.last.add_period(:from_date => Date.new(2000, 1, 1), :to_date => Date.new(2001, 1, 1),
+      :house => House.representatives, :division => "A", :party => "SPK", :count => 1)
+    
+    @people << Person.new(:name => Name.new(:first => "Joe", :last => "Smith"), :count => 2)
+    @people.last.add_period(:from_date => Date.new(2001, 1, 1), :to_date => Date.new(2002, 1, 1),
+      :house => House.representatives, :division => "B", :party => "SPK", :count => 2)
+
+    @people << Person.new(:name => Name.new(:first => "Henry", :last => "Smith"), :count => 3)
+    @people.last.add_period(:from_date => Date.new(2000, 1, 1), :to_date => Date.new(2001, 1, 1),
+      :house => House.representatives, :division => "C", :party => "CWM", :count => 3)
   end
   
-  it "can list all the electoral divisions for all the members" do
-    people = People.new
-
-    people << Person.new(:name => Name.new(:first => "John", :last => "Smith"), :count => 1)
-    people.last.add_period(:house => House.representatives, :division => "A", :count => 1)
-    
-    people << Person.new(:name => Name.new(:first => "Joe", :last => "Smith"), :count => 2)
-    people.last.add_period(:house => House.representatives, :division => "B", :count => 2)
-
-    people << Person.new(:name => Name.new(:first => "Henry", :last => "Smith"), :count => 3)
-    people.last.add_period(:house => House.representatives, :division => "C", :count => 3)
-    
-    people.divisions.should == %w{A B C}
+  it "can list all the electoral divisions for all the members" do    
+    @people.divisions.should == %w{A B C}
   end
   
   it "knows who the speaker is" do
-    member = @people.house_speaker(Date.new(2007, 10, 1))
-    member.person.name.full_name.should == "David Peter Maxwell Hawker"
+    member = @people.house_speaker(Date.new(2000, 6, 1))
+    member.person.name.full_name.should == "John Smith"
     member.should be_house_speaker
   end
 
   it "knows who the deputy speakers is" do
-    member = @people.deputy_house_speaker(Date.new(2008, 2, 12))
-    member.person.name.full_name.should == "Ms Anna Elizabeth Burke"
+    member = @people.deputy_house_speaker(Date.new(2000, 6, 1))
+    member.person.name.full_name.should == "Henry Smith"
     member.should be_deputy_house_speaker
   end
 end
