8b81c279fb2a3250b8d1d84c398be672fc40170d
Matthew Landauer <matthew@openaustralia.org>
2009-04-04 16:32:28 +1100

    Updated regression test to include new constituencies xml

--- regression-test/regression_test_parse_members.rb
+++ regression-test/regression_test_parse_members.rb
@@ -33,10 +33,11 @@ people = PeopleCSVReader.read_members
 PeopleCSVReader.read_all_ministers(people)
 puts "Writing XML..."
 people.write_xml("#{conf.members_xml_path}/people.xml", "#{conf.members_xml_path}/all-members.xml",
-  "#{conf.members_xml_path}/peers-ucl.xml", "#{conf.members_xml_path}/ministers.xml")
+  "#{conf.members_xml_path}/peers-ucl.xml", "#{conf.members_xml_path}/ministers.xml", "#{conf.members_xml_path}/constituencies.xml")
 
 ref_path = "#{File.dirname(__FILE__)}/../../ref"
 compare_xml("#{conf.members_xml_path}/people.xml", "#{ref_path}/people.xml")
 compare_xml("#{conf.members_xml_path}/all-members.xml", "#{ref_path}/all-members.xml")
 compare_xml("#{conf.members_xml_path}/peers-ucl.xml", "#{ref_path}/peers-ucl.xml")
 compare_xml("#{conf.members_xml_path}/ministers.xml", "#{ref_path}/ministers.xml")
+compare_xml("#{conf.members_xml_path}/constituencies.xml", "#{ref_path}/constituencies.xml")
