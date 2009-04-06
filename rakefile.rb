45b01d8840545c33af795d60cb1a0c9175358539
Matthew Landauer <matthew@openaustralia.org>
2009-04-06 11:22:14 +1000

    Turn off noisy Ruby warnings

--- rakefile.rb
+++ rakefile.rb
@@ -6,13 +6,11 @@ require 'spec/rake/spectask'
 task :default => [:spec]
 
 Spec::Rake::SpecTask.new do |t|
-    t.warning = true
     t.ruby_opts = ['-rtest/unit']
     t.spec_files = FileList['spec/*_spec.rb', 'test/test_*.rb']
 end
 
 Spec::Rake::SpecTask.new(:spec_coverage) do |t|
-    t.warning = true
     t.rcov = true
     t.rcov_opts = ["-x/Library/, -xspec"]
     t.ruby_opts = ['-rtest/unit']
