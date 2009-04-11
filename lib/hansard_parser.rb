7ee226ea8f793f97028f139c1a7a4907e4275abf
Matthew Landauer <matthew@openaustralia.org>
2009-04-11 23:16:18 +1000

    Fix obvious problems shown by errors for 2007

--- lib/hansard_parser.rb
+++ lib/hansard_parser.rb
@@ -248,7 +248,7 @@ class HansardParser
       unless HansardSpeech.generic_speaker?(speech.speakername)
         # It is so common that the problem with "The Temporary Chairman" occurs (where there real name is not included)
         # that we're going to downgrade this to a warning so that it doesn't drown out other problems
-        if ["The ACTING DEPUTY PRESIDENT", "The TEMPORARY CHAIRMAN", "The ACTING SPEAKER", "The Clerk", "The ACTING PRESIDENT", "DEPUTY SPEAKER, The"].include?(speech.speakername)
+        if ["The ACTING DEPUTY PRESIDENT", "The TEMPORARY CHAIRMAN", "The ACTING SPEAKER", "The Clerk", "The ACTING PRESIDENT", "DEPUTY SPEAKER, The", "DEPUTY CHAIR"].include?(speech.speakername)
           logger.warn "#{date} #{house}: Unknown speaker #{speech.speakername}"
         else
           logger.error "#{date} #{house}: Unknown speaker #{speech.speakername}"
