7ac0113e3b50d5780ad2079063b15649876780df
Matthew Landauer <matthew@openaustralia.org>
2009-04-18 16:03:16 +1000

    Very rough and ready scripts for importing/exporting comments when gid's might change

---
+++ import-comments.rb
@@ -0,0 +1,26 @@
+#!/usr/bin/env ruby
+
+$:.unshift "#{File.dirname(__FILE__)}/lib"
+
+require 'configuration'
+require 'mysql'
+require 'csv'
+
+conf = Configuration.new
+
+db = Mysql.real_connect(conf.database_host, conf.database_user, conf.database_password, conf.database_name)
+
+data = CSV.readlines("exported-comments.csv")
+data.each do |row|
+  comment_id, user_id, visible, modflagged, posted, hdate, comment_body, hansard_body = row
+  quoted = hansard_body.gsub('"', '\"')
+  res = db.query("select epobject.epobject_id from epobject, hansard where hansard.hdate=\"#{hdate}\" and epobject.body=\"#{quoted}\" and epobject.epobject_id = hansard.epobject_id")
+  raise "More than one match for text: #{hansard_body} in comment_id: #{comment_id}" if res.num_rows > 1
+  epobject_id = res.fetch_hash["epobject_id"]
+
+  gid = db.query("select gid from hansard where epobject_id=\"#{epobject_id}\"").fetch_hash["gid"]
+
+  # Reinsert the comments back into the database
+  quoted = comment_body.gsub('"', '\"')
+  db.query("insert into comments (comment_id, user_id, epobject_id, body, posted, modflagged, visible, original_gid) VALUES (\"#{comment_id}\", \"#{user_id}\", \"#{epobject_id}\", \"#{quoted}\", \"#{posted}\", \"#{modflagged}\", \"#{visible}\", \"#{gid}\")")
+end
