308627e2375dd341beb52d6829d6d4a465ffda91
Matthew Landauer <matthew@openaustralia.org>
2009-10-23 11:52:17 +0100

    Downloads all the members data for QLD and handles multiple date ranges for a single member

--- qld/parse-members.rb
+++ qld/parse-members.rb
@@ -13,30 +13,42 @@ agent = MechanizeProxy.new
 #require 'nokogiri'
 #require 'open-uri'
 
-page = agent.get("http://www.parliament.qld.gov.au/view/historical/records1860.asp?SubArea=register_A")
-#page = Nokogiri::HTML(open("http://www.parliament.qld.gov.au/view/historical/records1860.asp?SubArea=register_A")) 
-
-page.search(".normal table").first.search("tr")[1..-1].each do |row|
-  elements = []
-  e = row.search("td")[0]
-  # Remove badly formatted td tags that lie within td tags
-  removed = e.search('td')
-  removed.remove
-  elements << e
-  name = elements[0].inner_text.strip
-  #p name
-  elements += removed
-  elements += row.search("td")[1..-1]
-  party = elements[1].inner_html.gsub("&nbsp;", "").strip
-  term = elements[2].inner_html.gsub("&nbsp;", "")
-  if term =~ /^(\d{2}.\d{2}.\d{4})\s*-\s*(\d{2}.\d{2}.\d{4})?$/
-    from_date = $~[1]
-    to_date = $~[2]
-  elsif term == ""
-    from_date = nil
-    to_date = nil
-  else
-    puts "**** Ignoring the term '#{term}' because it's not in an expected form"
+('A'...'Z').each do |letter|
+  begin
+    page = agent.get("http://www.parliament.qld.gov.au/view/historical/records1860.asp?SubArea=register_#{letter}&SubNav=register_A")
+    #page = Nokogiri::HTML(open("http://www.parliament.qld.gov.au/view/historical/records1860.asp?SubArea=register_A")) 
+  rescue WWW::Mechanize::ResponseCodeError => e
+    puts "WARNING: Could not get member page for letter #{letter}. Skipping."
+    next
   end
-  puts "name: #{name}, party: #{party}, from_date: #{from_date}, to_date: #{to_date}"
+
+  page.search(".normal table").first.search("tr")[1..-1].each do |row|
+    elements = []
+    e = row.search("td")[0]
+    # Remove badly formatted td tags that lie within td tags
+    removed = e.search('td')
+    removed.remove
+    elements << e
+    name = elements[0].inner_text.strip
+    #p name
+    elements += removed
+    elements += row.search("td")[1..-1]
+    party = elements[1].inner_html.gsub("&nbsp;", "").strip
+    terms = elements[2].inner_html.gsub("&nbsp;", "")
+    date_ranges = terms.split("<br />").map do |term|
+      if term =~ /^\s*(\d{2}.\d{2}.\d{4})\s*-\s*(\d{2}.\d{2}.\d{4})?( \(resigned\))?$/
+        from_date = $~[1]
+        to_date = $~[2]
+      elsif term == ""
+        from_date = nil
+        to_date = nil
+      else
+        puts "WARNING: Ignoring the term '#{term}' because it's not in an expected form"
+      end
+      [from_date, to_date]
+    end
+    date_ranges.each do |date_range|
+      puts "name: #{name}, party: #{party}, from_date: #{date_range[0]}, to_date: #{date_range[1]}"
+    end
+  end
 end
