6c99f4b7b52c317b6d35ddfb1ac8a5ca8a2b6af7
Matthew Landauer <matthew@openaustralia.org>
2009-10-25 11:23:12 +0000

    Separated scraping and output of data

--- qld/parse-members.rb
+++ qld/parse-members.rb
@@ -12,10 +12,15 @@ agent = MechanizeProxy.new
 #agent = WWW::Mechanize.new
 #require 'nokogiri'
 #require 'open-uri'
+require 'uri'
+require 'ostruct'
+
+members = []
 
 ('A'...'Z').each do |letter|
+  url = "http://www.parliament.qld.gov.au/view/historical/records1860.asp?SubArea=register_#{letter}&SubNav=register_A"
   begin
-    page = agent.get("http://www.parliament.qld.gov.au/view/historical/records1860.asp?SubArea=register_#{letter}&SubNav=register_A")
+    page = agent.get(url)
     #page = Nokogiri::HTML(open("http://www.parliament.qld.gov.au/view/historical/records1860.asp?SubArea=register_A")) 
   rescue WWW::Mechanize::ResponseCodeError => e
     puts "WARNING: Could not get member page for letter #{letter}. Skipping."
@@ -23,41 +28,48 @@ agent = MechanizeProxy.new
   end
 
   page.search(".normal table").first.search("tr")[1..-1].each do |row|
+    member = OpenStruct.new
+    
     elements = []
     e = row.search("td")[0]
     # Remove badly formatted td tags that lie within td tags
     removed = e.search('td')
     removed.remove
     elements << e
-    name = elements[0].inner_text.strip
-    #p name
+    member.name = elements[0].inner_text.strip
+    member.bio_url = URI.parse(url) + URI.parse(elements[0].at('a').attributes['href']) if elements[0].at('a')
     elements += removed
     elements += row.search("td")[1..-1]
-    party = elements[1].inner_html.gsub("&nbsp;", "").strip
+    member.party = elements[1].inner_html.gsub("&nbsp;", "").strip
     terms = elements[2].inner_html.gsub("&nbsp;", "")
-    date_ranges = terms.split("<br />").map do |term|
+    member.date_ranges = terms.split("<br />").map do |term|
       if term =~ /^\s*(\d+\.\d+\.\d+)\s*-\s*(\d+\.\d+\.\d+)?( \(resigned\))?$/
         begin
           from_date = Date.parse($~[1])        
         rescue ArgumentError
-          puts "WARNING: #{name} has invalid date in #{$~[1]}"
+          puts "WARNING: #{member.name} has invalid date in #{$~[1]}"
         end
 
         begin
           to_date = Date.parse($~[2]) if $~[2]
         rescue ArgumentError
-          puts "WARNING: #{name} has invalid date in #{$~[2]}"
+          puts "WARNING: #{member.name} has invalid date in #{$~[2]}"
         end
       elsif term == ""
         from_date = nil
         to_date = nil
       else
-        puts "WARNING: For #{name} ignoring the term '#{term}' because it's not in an expected form"
+        puts "WARNING: For #{member.name} ignoring the term '#{term}' because it's not in an expected form"
       end
-      [from_date, to_date]
+      OpenStruct.new(:start => from_date, :end => to_date)
     end
-    date_ranges.each do |date_range|
-      puts "name: #{name}, party: #{party}, from_date: #{date_range[0]}, to_date: #{date_range[1]}"
-    end
+    members << member
+  end
+  
+end
+
+members.each do |m|
+  m.date_ranges.each do |date_range|
+    puts "name: #{m.name}, party: #{m.party}, from_date: #{date_range.start}, to_date: #{date_range.end}, bio_url: #{m.bio_url}"
   end
 end
