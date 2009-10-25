c7eb7a7d1fd6dc3100068e0e519bc4e1ff44792b
Matthew Landauer <matthew@openaustralia.org>
2009-10-25 19:11:02 +0000

    Also simplify the names in representatives.csv and senators.csv

--- compress_names.rb
+++ compress_names.rb
@@ -19,7 +19,6 @@ CSV.open("data/people2.csv", "w") do |writer|
       # This line is a comment. Should output identically
       writer << line.compact
     else
-      p line
       person_count, aph_id, title, lastname, firstname, middlename, post_title, birthday = line[0..7]
       name = Name.new(:last => lastname, :first => firstname, :middle => middlename, :title => title, :post_title => post_title)
 
@@ -33,6 +32,50 @@ CSV.open("data/people2.csv", "w") do |writer|
       end
       
       writer << [person_count, aph_id, name.full_name, birthday] + alternate_names.map{|n| n.full_name}
+    end
+  end
+end
+
+data = CSV.readlines("data/representatives.csv")
+data.shift
+data.shift
+
+CSV.open("data/representatives2.csv", "w") do |writer|
+  writer << ["member count","person count","name","Division","State/Territory","Date of election","Type of election","Date ceased to be a Member","reason","Most recent party"]
+  writer << []
+  
+  data.each do |line|
+    if line[0][0..0] == '#'
+      # This line is a comment. Should output identically
+      writer << line.compact
+    else
+      member_count, person_count, title, lastname, firstname, middlename, house, division, state, start_date, start_reason, end_date, end_reason, party = line
+      name = Name.new(:last => lastname, :first => firstname, :middle => middlename, :title => title)
+      
+      raise "Unexpected value for house" unless house == "representatives"
+      writer << [member_count, person_count, name.full_name, division, state, start_date, start_reason, end_date, end_reason, party]
+    end
+  end
+end
+
+data = CSV.readlines("data/senators.csv")
+data.shift
+data.shift
+
+CSV.open("data/senators2.csv", "w") do |writer|
+  writer << ["member count","person count","name","Division","State/Territory","Date of election","Type of election","Date ceased to be a Member","reason","Most recent party"]
+  writer << []
+  
+  data.each do |line|
+    if line[0][0..0] == '#'
+      # This line is a comment. Should output identically
+      writer << line.compact
+    else
+      member_count, person_count, title, lastname, firstname, middlename, house, division, state, start_date, start_reason, end_date, end_reason, party = line
+      name = Name.new(:last => lastname, :first => firstname, :middle => middlename, :title => title)
+      
+      raise "Unexpected value for house" unless house == "senate"
+      writer << [member_count, person_count, name.full_name, division, state, start_date, start_reason, end_date, end_reason, party]
     end
   end
 end
