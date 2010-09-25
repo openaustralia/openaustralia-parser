c5b2231c36f7b9bf393cf5808b6ee6ecd624cc1c
Matthew Landauer <matthew@openaustralia.org>
2010-09-25 17:52:40 +1000

    Lock to specific version of activesupport to make mb_chars work on parse-members.rb

--- lib/environment.rb
+++ lib/environment.rb
@@ -2,7 +2,7 @@
 # This avoids spreading it out throughout the codebase
 
 require 'rubygems'
-gem 'activesupport', ">= 2.2"
+gem 'activesupport', "= 2.3.7"
 gem 'mechanize', "= 0.9.2"
 gem 'htmlentities'
 
