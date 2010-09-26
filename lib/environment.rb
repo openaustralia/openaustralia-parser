a94ce5043b9cb9b119e3c9237c7b3ca99ff758f1
Matthew Landauer <matthew@openaustralia.org>
2010-09-26 10:58:21 +1000

    Force using this version of hpricot so Marshal.dump of PageProxy object doesn't fail. Ugh.

--- lib/environment.rb
+++ lib/environment.rb
@@ -4,6 +4,8 @@
 require 'rubygems'
 gem 'activesupport', "= 2.3.7"
 gem 'mechanize', "= 0.9.2"
+# Force using this version of hpricot so Marshal.dump of PageProxy object doesn't fail. Ugh.
+gem 'hpricot', "= 0.6.164"
 gem 'htmlentities'
 
 gem 'builder'
