58c9de9a83d3178ce8dca0de3ca8fc0f7072f95a
Henare Degan <henare.degan@gmail.com>
2011-03-21 16:43:10 +1100

    Workaround an incompatibility between Rails 2.3.8 and recent versions of RubyGems by not requiring a specific, old version of activesupport
    See: http://stackoverflow.com/questions/5176782/uninitialized-constant-activesupportdependenciesmutex-nameerror

--- lib/environment.rb
+++ lib/environment.rb
@@ -2,7 +2,7 @@
 # This avoids spreading it out throughout the codebase
 
 require 'rubygems'
-gem 'activesupport', "= 2.3.7"
+gem 'activesupport'
 gem 'mechanize', "= 0.9.2"
 # Force using this version of hpricot so Marshal.dump of PageProxy object doesn't fail. Ugh.
 gem 'hpricot', "= 0.6.164"
