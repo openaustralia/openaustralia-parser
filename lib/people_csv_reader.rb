5220dfef0c9ec1281978d16d4becc2620e11f6f1
Henare Degan <henare.degan@gmail.com>
2012-02-07 13:37:42 +1100

    OA-495 Correct the party of John Madigan

--- lib/people_csv_reader.rb
+++ lib/people_csv_reader.rb
@@ -149,6 +149,8 @@ class PeopleCSVReader
       "Nuclear Disarmament Party"
     when "PHON"
       "Pauline Hanson's One Nation Party"
+    when "DLP"
+      "Democratic Labor Party"
     when "ANTI-SOC", "SPK", "CWM", "PRES", "DPRES"
       # Do nothing
       party
