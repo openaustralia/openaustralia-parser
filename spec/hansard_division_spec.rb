aa4d65bd61a9285980f8cd111372c96ceb965d53
Henare Degan <henare.degan@gmail.com>
2014-09-12 10:40:15 +1000

    Use RSpec subject instead of setting an instance variable

--- spec/hansard_division_spec.rb
+++ spec/hansard_division_spec.rb
@@ -6,9 +6,9 @@ require "hansard_division"
 require 'hpricot'
 
 describe HansardDivision do
-  before :each do
+  subject(:division) do
     # Tellers are indicated with a "*". There might be or might not be a space before the "*".
-    @division = HansardDivision.new(Hpricot.XML('
+    HansardDivision.new(Hpricot.XML('
     <division>
 			<division.header>
 				<time.stamp>09:06:00</time.stamp>
@@ -42,17 +42,17 @@ describe HansardDivision do
   
   it "should parse the xml for a division correctly" do
 		# Note that the *'s are stripped from the names
-		@division.yes.should == ["Joe Bloggs", "Henry Smith", "Phil Smith"]
-		@division.no.should == ["John Smith"]
+		division.yes.should == ["Joe Bloggs", "Henry Smith", "Phil Smith"]
+		division.no.should == ["John Smith"]
   end
   
   it "should know the time the division took place" do
-		@division.time.should == "09:06:00"
+		division.time.should == "09:06:00"
   end
 
   it "should recognise the tellers" do
-    @division.yes_tellers == ["Joe Bloggs", "Phil Smith"]
-    @division.no_tellers == ["John Smith"]
+    division.yes_tellers == ["Joe Bloggs", "Phil Smith"]
+    division.no_tellers == ["John Smith"]
   end  
 
   describe 'tied vote' do
