3f380d278a90c5b6bab891282e085310da83d3c3
Joel Nothman <joel.nothman@gmail.com>
2017-07-19 00:44:52 +0000

    Allow retry without patch on error (for network/server problems)

--- parse-speeches.rb
+++ parse-speeches.rb
@@ -93,10 +93,13 @@ def parse_with_retry(interactive, parse, date, path, house)
       raise
     end
     while 1
-      print "Patch / Continue / Quit? "
+      print "Retry / Patch / Continue / Quit? "
       choice = STDIN.gets.upcase[0..0]
       if choice == "P"
         system "#{File.dirname(__FILE__)}/create_patch.rb #{house} #{date}"
+        parse_with_retry parse, date, path, house
+        break
+      elsif choice == 'R':
         parse_with_retry parse, date, path, house
         break
       elsif choice == 'C'
