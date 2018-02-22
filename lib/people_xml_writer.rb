4eb5582e7fe6bd576b409883ebd16805204532c3
Matthew Landauer <mlandauer@gmail.com>
2018-02-23 10:31:38 +1100

    whitespace changes only

--- lib/people_xml_writer.rb
+++ lib/people_xml_writer.rb
@@ -2,7 +2,7 @@ require 'builder_alpha_attributes'
 require 'configuration'
 
 class PeopleXMLWriter
-  
+
   def PeopleXMLWriter.write(people, people_filename, members_filename, senators_filename, ministers_filename, divisions_filename)
     conf = Configuration.new
 
@@ -12,21 +12,21 @@ class PeopleXMLWriter
     write_ministers(people, ministers_filename)
     File.open(divisions_filename, 'w') {|f| write_divisions(people, f)}
   end
-  
+
   def self.write_divisions(people, output)
     x = Builder::XmlMarkup.new(:target => output, :indent => 2)
     x.divisions do
       people.divisions.each_with_index do |division, index|
         x.division(:fromdate => "1000-01-01", :id => "uk.org.publicwhip/cons/#{index + 1}", :todate => "9999-12-31") do
           x.name(:text => division)
-        end        
+        end
       end
     end
   end
-  
+
   def PeopleXMLWriter.write_ministers(people, filename)
     conf = Configuration.new
-    
+
     xml = File.open(filename, 'w')
     x = Builder::XmlMarkup.new(:target => xml, :indent => 1)
     x.instruct!
@@ -39,7 +39,7 @@ class PeopleXMLWriter
               :matchid => person.periods.first.id, :position => p.position,
               :fromdate => p.from_date, :todate => p.to_date, :dept => "", :source => "")
           end
-        end  
+        end
       end
     end
     xml.close
@@ -61,7 +61,7 @@ class PeopleXMLWriter
 
   def PeopleXMLWriter.write_members(people, filename)
     conf = Configuration.new
-    
+
     xml = File.open(filename, 'w')
     x = Builder::XmlMarkup.new(:target => xml, :indent => 1)
     x.instruct!
@@ -90,7 +90,7 @@ class PeopleXMLWriter
 
   def PeopleXMLWriter.write_senators(people, filename)
     conf = Configuration.new
-    
+
     xml = File.open(filename, 'w')
     x = Builder::XmlMarkup.new(:target => xml, :indent => 1)
     x.instruct!
@@ -99,17 +99,17 @@ class PeopleXMLWriter
         person.senate_periods.each do |period|
           x.member(:id => period.id,
             :house => "senate", :title => period.person.name.title, :firstname => period.person.name.first,
-            :lastname => period.person.name.last, :division => period.state, :party => period.party,    
+            :lastname => period.person.name.last, :division => period.state, :party => period.party,
             :fromdate => period.from_date, :todate => period.to_date, :fromwhy => period.from_why, :towhy => period.to_why)
         end
       end
     end
     xml.close
   end
-  
+
   def PeopleXMLWriter.write_people(people, filename)
     conf = Configuration.new
-    
+
     xml = File.open(filename, 'w')
     x = Builder::XmlMarkup.new(:target => xml, :indent => 1)
     x.instruct!
@@ -138,7 +138,7 @@ class PeopleXMLWriter
             end
           end
         end
-      end  
+      end
     end
     xml.close
   end
