3127a397bb8301c1950d835a0edc45376551a54c
Matthew Landauer <mlandauer@gmail.com>
2021-01-06 09:59:57 +1100

    Use YAML::safe_load

--- lib/configuration.rb
+++ lib/configuration.rb
@@ -26,7 +26,7 @@ class Configuration
     if @@conf.nil?
       puts "Loading config from: #{File.dirname(__FILE__)}/../configuration.yml"
       # Load the configuration from the config file
-      @@conf = YAML::load(File.open("#{File.dirname(__FILE__)}/../configuration.yml"))
+      @@conf = YAML::safe_load(File.open("#{File.dirname(__FILE__)}/../configuration.yml"))
       @@conf = {} if !@@conf
     end
     if conf.nil?
