4116a518110b31847baa17158f422991b4b3a86e
Ian Heggie (OAF) <ian+oaf@heggie.biz>
2026-03-10 17:13:10 +1100

    Add clear explicit spec feedback

---
+++ test/test_helper.rb
@@ -0,0 +1,20 @@
+# Common spec helper
+
+$LOAD_PATH.unshift File.expand_path("../lib", __dir__)
+
+require "bundler/setup"
+
+require "simplecov"
+require "simplecov-console"
+
+SimpleCov.start do
+  add_filter "/spec/"
+  add_filter "/test/"
+end
+
+SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new(
+  [
+    SimpleCov::Formatter::HTMLFormatter,
+    SimpleCov::Formatter::Console
+  ]
+)
