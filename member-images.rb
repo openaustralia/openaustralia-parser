bee1f832362b49a5d7cd7ae8fdcdd7b13b25529c
Ian Heggie (OAF) <ian+oaf@heggie.biz>
2026-03-12 17:51:58 +1100

    Write spec for member-images, adding limit and output-dir options

--- member-images.rb
+++ member-images.rb
@@ -3,25 +3,38 @@
 
 $LOAD_PATH.unshift "#{File.dirname(__FILE__)}/lib"
 
+require "optparse"
 require "configuration"
 require "people"
 
 class MemberImages
   def initialize(args)
     @args = args
+    @options = { limit: nil }
+    OptionParser.new do |opts|
+      opts.banner = "Usage: member-images.rb [--limit=N] [--output-dir=PATH]"
+      opts.on("--limit=N", Integer, "Cap the number of people whose images are downloaded") do |n|
+        @options[:limit] = n
+      end
+      opts.on("--output-dir=PATH", "Download images to PATH instead of conf.file_image_path") do |path|
+        @options[:output_dir] = path
+      end
+    end.parse!(@args)
   end
 
   def run
     conf = Configuration.new
     people = PeopleCSVReader.read_members
-    FileUtils.mkdir_p ["#{conf.file_image_path}/mps",
-                       "#{conf.file_image_path}/mpsL",
-                       "#{conf.file_image_path}/mpsXL"]
-    puts "Downloading person images to #{conf.file_image_path}/{mps,mpsL,mpsXL}..."
+    file_image_path = @options[:output_dir] || conf.file_image_path
+    FileUtils.mkdir_p ["#{file_image_path}/mps",
+                       "#{file_image_path}/mpsL",
+                       "#{file_image_path}/mpsXL"]
+    puts "Downloading person images to #{file_image_path}/{mps,mpsL,mpsXL}..."
     people.download_images(
-      "#{conf.file_image_path}/mps",
-      "#{conf.file_image_path}/mpsL",
-      "#{conf.file_image_path}/mpsXL"
+      "#{file_image_path}/mps",
+      "#{file_image_path}/mpsL",
+      "#{file_image_path}/mpsXL",
+      limit: @options[:limit]
     )
     puts "Finished downloading person images!"
   end
